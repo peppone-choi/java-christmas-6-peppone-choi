ea78d79d201ffebbdb31e637889d88eb2bec8d11
peppone-choi <peppone.choi@gmail.com>
2023-11-11 03:17:44 +0900

    feat: 주문 메뉴와 갯수의 입력 및 출력, Valid 구현

--- src/main/java/christmas/model/BeforeEventPrice.java
+++ src/main/java/christmas/model/BeforeEventPrice.java
@@ -6,7 +6,6 @@ public class BeforeEventPrice {
     private final int price;
 
     public BeforeEventPrice(Orders orders) {
-        validBeforePrice(orders.calculateAll());
         this.price = orders.calculateAll();
     }
     public int printBeforePrice() {
