be5876e76b55f24185c2b4e616a216cff5829b6f
peppone-choi <peppone.choi@gmail.com>
2023-11-12 18:59:55 +0900

    refactor: DI(의존성 역전) 적용

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -3,9 +3,14 @@ package christmas;
 import christmas.controller.ChristmasController;
 import christmas.view.Impl.InputViewImpl;
 import christmas.view.Impl.OutputViewImpl;
+import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        new ChristmasController(new InputViewImpl(), new OutputViewImpl()).run();
+        InputView inputView = new InputViewImpl();
+        OutputView outputView = new OutputViewImpl();
+
+        new ChristmasController(inputView, outputView).run();
     }
 }
