c15fb19450305ee4c3b0887d723d87d3890b8793
peppone-choi <peppone.choi@gmail.com>
2023-11-12 14:37:46 +0900

    refactor: 상수를 최대한 ENUM으로

--- src/main/java/christmas/config/CommonConfig.java
+++ src/main/java/christmas/config/CommonConfig.java
@@ -14,7 +14,7 @@ public enum CommonConfig {
     PLUS_ENTER_FORMAT("%s\n%n"),
     DISCOUNT_FORMAT("{0}: -{1}원 \n"),
     PLEASE_INPUT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
-    PLASE_INPUT_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    PLEASE_INPUT_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
     PRINT_DATE_FORMAT("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n%n"),
     ORDERED_MENU("<주문 메뉴>"),
     BEFORE_EVENT_PRICE("<할인 전 총주문 금액>"),
--- src/main/java/christmas/view/Impl/InputViewImpl.java
+++ src/main/java/christmas/view/Impl/InputViewImpl.java
@@ -1,7 +1,7 @@
 package christmas.view.Impl;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
-import static christmas.config.CommonConfig.PLASE_INPUT_ORDER;
+import static christmas.config.CommonConfig.PLEASE_INPUT_ORDER;
 import static christmas.config.CommonConfig.PLEASE_INPUT_DATE;
 
 import christmas.util.Validation;
@@ -25,7 +25,7 @@ public class InputViewImpl implements InputView {
     public String readOrder() {
         while (true) {
             try {
-                System.out.println(PLASE_INPUT_ORDER.getString());
+                System.out.println(PLEASE_INPUT_ORDER.getString());
                 return returnOrders(readLine());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
